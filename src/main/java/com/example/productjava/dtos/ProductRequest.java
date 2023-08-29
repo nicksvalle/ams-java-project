1bf8b02064074b093a7545a1a3d59527fad0e8a5
nicksvalle <nicolasdeoliveiravalle@gmail.com>
2023-08-29 15:32:39 -0300

    finalizado

--- src/main/java/com/example/productjava/dtos/ProductRequest.java
+++ src/main/java/com/example/productjava/dtos/ProductRequest.java
@@ -1,7 +1,14 @@
 package com.example.productjava.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public record ProductRequest(
+
+    @NotBlank(message = "Nome não pode ser em branco")
     String name,
+    
+    @Min(value = 0, message = "O valor mínimo para o preço é zero")
     double price
 ) {
 
--- src/main/java/com/example/productjava/resources/ProductController.java
+++ src/main/java/com/example/productjava/resources/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,7 +48,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductResponse> save(@RequestBody ProductRequest product){
+    public ResponseEntity<ProductResponse> save(@Validated @RequestBody ProductRequest product){
         var savedProduct = this.service.save(product);
 
         URI location = ServletUriComponentsBuilder
